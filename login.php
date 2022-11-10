fdc9abd6936a659853155f3bb12647f5117aecba
gabriel <108320347+gabrielplus@users.noreply.github.com>
2022-11-10 11:39:38 +0300

    done with signing up codes

--- login.php
+++ login.php
@@ -1,21 +1,75 @@
+<?php
+
+@include 'configure.php';
+
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $waste_type = $_POST['waste_type'];
+
+   $select = " SELECT * FROM waste WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
+
+      if($row['waste_type'] == 'Home'){
+
+         $_SESSION['Home_name'] = $row['name'];
+         header('location:index.html');
+
+      }elseif($row['waste_type'] == 'Industrial'){
+
+         $_SESSION['Industrial_name'] = $row['name'];
+         header('location:index.html');
+
+      }
+     
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+
+};
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login form</title>
-    <link rel="stylesheet" href="assets\css\reg.css">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="assets/css/reg.css">
+
 </head>
 <body>
-    <div class="form-container">
-        <form action="" method="post">
-           <h3>login now</h3>            
-           <input type="email" name="email" required placeholder="Enter your email">
-           <input type="password" name="password" required placeholder="Enter your password">
-           <input type="submit" name="submit" value="login now now" class="form-btn">
-           <p>don't have an account at E-waste? <a href="user.php">Register now</a></p>
-        </form>
-    </div>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+      <p>don't have an account? <a href="register_form.php">register now</a></p>
+   </form>
+
+</div>
+
 </body>
 </html>
--- user.php
+++ user.php
@@ -1,3 +1,39 @@
+<?php
+
+@include 'configure.php' ;
+
+if(isset($_POST['submit'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = md5($_POST['password']);
+    $cpass = md5($_POST['cpassword']);
+    $waste_type = $_POST['waste_type'];
+
+    $select = " SELECT * FROM waste WHERE email = '$email' && password = '$pass' ";
+
+    $result = mysqli_query($conn, $select);
+
+    if(mysqli_num_rows($result) > 0) {
+
+        $error[] = 'User already exist!';
+
+    }else {
+
+        if($pass != $cpass) {
+            $error[] = 'Password not matched!';
+        }else{
+            $insert = "INSERT INTO waste(name, email, password, waste_type) VALUES('$name','$email','$pass','$waste_type')";
+            mysqli_query($conn, $insert);
+            header('location:login.php');
+        }
+    }
+
+};
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,13 +47,24 @@
     <div class="form-container">
         <form action="" method="post">
            <h3>Deposit E-waste now</h3>
+           <?php
+
+           if(isset($error)){
+            foreach($error as $error){
+                echo '<span class="error-msg">'.$error.'</span>';
+                             
+            }
+           }
+
+           ?>
+
            <input type="text" name="name" required placeholder="Enter your name"> 
            <input type="email" name="email" required placeholder="Enter your email">
            <input type="password" name="password" required placeholder="Enter your password">
            <input type="password" name="cpassword" required placeholder="confirm your password">
-           <select name="user_type">
-                <option value="user">user</option>
-                <option value="admin">admin</option>
+           <select name="waste_type">
+                <option value="Home">Home</option>
+                <option value="Industrial">Industrial</option>
            </select>
            <input type="submit" name="submit" value="register now" class="form-btn">
            <p>already have an account at E-waste? <a href="login.php">login now</a></p>
